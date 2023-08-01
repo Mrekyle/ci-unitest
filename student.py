66a36343d1a241c490edb0dad83c9e07fe780d6b
Kyle Chart <64297454+Mrekyle@users.noreply.github.com>
2023-08-01 20:45:29 +0100

    Add mocking api requests testing

--- student.py
+++ student.py
@@ -1,4 +1,5 @@
 from datetime import date, timedelta
+import requests
 
 class Student:
     """
@@ -18,17 +19,32 @@ class Student:
         """
         Returns the full name of the student 
         """
-        @property
-        def full_name(self):
-            return f"{self._first_name} {self._last_name}"
+    @property
+    def full_name(self):
+        return f"{self._first_name} {self._last_name}"
 
-        def alert_santa(self):
-            self.naughty_list = True  
+    def alert_santa(self):
+        self.naughty_list = True  
 
-        """
-        As its a read only method the property decorator indicates
-        this
-        """
-        @property
-        def student_email(self):
-            return f"{self._first_name.lower()}.{self._last_name.lower()}@email.com"
+    """
+    As its a read only method the property decorator indicates
+    this
+
+    Using the .lower() method as all email address use lowercase letters
+    """
+    @property
+    def student_email(self):
+        return f"{self._first_name.lower()}.{self._last_name.lower()}@email.com"
+
+    def apply_extension(self, days):
+        self.end_date = self.end_date + timedelta(days=5)
+
+    def course_schedule(self):
+        response = requests.get(f"http://www.mycompanyname.com/course-schedule/{self._last_name}/{self._first_name}")
+
+        if response.ok:
+            return response.text 
+        else:
+            return "Something went wrong here"
+
+  
--- test_student.py
+++ test_student.py
@@ -1,10 +1,32 @@
 import unittest
 from student import Student
+from datetime import timedelta
+from unittest.mock import patch
 
 class TestStudent(unittest.TestCase):
     """
     Testing the student class in the student.py file
     """
+
+    """
+    The general set up function for the start of the testing process
+    allows the developer to define variables that are to be used 
+    over and over again. Meaning no repeated code. 
+    
+    Such as defining the instance of the student to be used in all 
+    the tests 
+    """
+    def setUp(self):
+        print('Setting up the testing process')
+        self.student = Student('John', 'Doe')
+
+    """
+    The tear down function is the end of the testing process.
+    This will essentially reset anything/delete anything that was
+    created during the testing process. 
+    """
+    def tearDown(self):
+        print('Tear Down Function')
 
     def test_full_name(self):
         """
@@ -13,9 +35,14 @@ class TestStudent(unittest.TestCase):
         
         Then creating an instance of the student data for testing 
         purposes. 
+
+        Adding the print statement to see what test has been run
+        and when it has been run. Allowing more visibility to the 
+        testing process in the terminal 
         """
-        student = Student('John', 'Doe')
-        self.assertEqual(student.full_name, 'John Doe') 
+        print('Testing Full Name')
+        #  student = Student('John', 'Doe')
+        self.assertEqual(self.student.full_name, 'John Doe') 
         
     def test_alert_santa(self):
         """
@@ -25,19 +52,62 @@ class TestStudent(unittest.TestCase):
         First creating an instance of the student for testing
         purposes
         """
-        student = Student('John', 'Doe')
-        student.alert_santa()
+        print('Testing Alert Santa')
+        #  student = Student('John', 'Doe')
+        self.student.alert_santa()
         
         """
         No need to pass the True or False check. Due to the 
         assertTrue method only looking for a True statement
         assertFalse does the same for the False statement 
         """
-        self.assertTrue(student.naughty_list)
+        self.assertTrue(self. student.naughty_list)
 
     def test_student_email(self):
-        student = Student('John', 'Doe')
-        self.assertEqual(student.student_email, 'john.doe@email.com')
+        """
+        Testing the students email address by checking if the 
+        outputted value is equal to the string defined in the 
+        student class from the other file
+        """
+        print('Testing Student Email')
+        #  student = Student('John', 'Doe')
+        self.assertEqual(self.student.student_email, 'john.doe@email.com')
+
+    def test_apply_extension(self):
+        old_end_date = self.student.end_date
+        self.student.apply_extension(5)
+
+        self.assertEqual(self.student.end_date, old_end_date + timedelta(days=5))
+
+    def test_course_schedule_success(self):
+        print('Testing course Schedule')
+        """
+        Using the patch method from unittest mock we can mock requests from things
+        such as an api/database by filling the date inside of the function
+        """
+        with patch("student.requests.get") as mocked_get:
+            # Mocking the response from an api
+            mocked_get.returned_value.ok = True
+            # mMocking the text response from an api
+            mocked_get.returned_value.text = 'Success'
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, 'Success')
+    
+    def test_course_schedule_fail(self):
+        """
+        Testing if the response from the api has failed by using the
+        mock testing method
+        """
+        print('Testing course schedule fail response')
+
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.returned_value.ok = False
+            mocked_get.returned_value.text = 'Something has gone wrong here'
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, 'Something has gone wrong here')
+
 
 if __name__ == '__main__':
     unittest.main()
